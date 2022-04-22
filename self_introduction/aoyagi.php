d5efd735431b4a70abd9eb46be7ef331dba4f54c
tri-aoyagi <aoyagi_saori@3-ize.jp>
2022-04-22 11:57:55 +0900

    自己紹介

---
+++ self_introduction/aoyagi.php
@@ -0,0 +1,43 @@
+<?php
+
+// ここに自己紹介を入力してください。(各項目255文字以内)
+$name = '青柳早織';
+$favorite = 'KPOP';
+$aspiration = '仕事を早く覚える';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h2>自己紹介</h2>
+
+        <h3>名前</h3>
+        <!-- 下の<p>と</p>の間に名前($name)が表示されるようにしてください -->
+        <p>
+        <?php
+        echo $name;
+        ?>
+        </p>
+
+        <h3>好きなこと</h3>
+        <!-- 下の<p>と</p>の間に好きなこと($favorite)が表示されるようにしてください -->
+        <p>
+            <?php
+            echo $favorite;
+            ?> 
+        </p>
+
+        <h3>抱負</h3>
+        <!-- 下の<p>と</p>の間に抱負($aspiration)が表示されるようにしてください -->
+        <p>
+            <?php
+            echo $aspiration;
+            ?>
+        </p>
+        
+    </body>
+</html>
