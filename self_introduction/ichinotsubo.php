43e462435a047234505d194fb361c0c9ac30bea4
ichinotsubo <ichinotsubo@3-ize.jp>
2022-04-22 12:03:55 +0900

    研修ファイル追加

---
+++ self_introduction/ichinotsubo.php
@@ -0,0 +1,38 @@
+<?php
+
+// ここに自己紹介を入力してください。(各項目255文字以内)
+$name = '一ノ坪';
+$favorite = 'ライブいくこと';
+$aspiration = '痩せる';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h2>自己紹介</h2>
+
+        <h3>名前</h3>
+        <!-- 下の<p>と</p>の間に名前($name)が表示されるようにしてください -->
+        <?php echo $name ?>です
+        <p>
+        
+        </p>
+
+        <h3>好きなこと</h3>
+        <!-- 下の<p>と</p>の間に好きなこと($favorite)が表示されるようにしてください -->
+        <p>
+        <?php echo $favorite ?>です
+        </p>
+
+        <h3>抱負</h3>
+        <!-- 下の<p>と</p>の間に抱負($aspiration)が表示されるようにしてください -->
+        <p>
+        <?php echo $aspiration ?>です
+        </p>
+        
+    </body>
+</html>
